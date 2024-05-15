bc99e7b5a04d902e62d60060aaca735d886832ae
Harsh Maurya <mauryaharsh464@gmail.com>
2024-05-15 12:47:57 +0530

    cloudinery multer

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,16 +3,16 @@ class ApiError extends Error {
     statusCode,
     message = "something went wrong",
     errors = [],
-    statck = ""
+    stack = ""
   ) {
     super(message);
     this.statusCode = statusCode;
-    this.data = null; 
+    this.data = null;
     this.message = message;
     this.success = false;
     this.errors = errors;
-    if (statck) {
-      this.stack = statck;
+    if (stack) {
+      this.stack = stack;
     } else {
       Error.captureStackTrace(this, this.constructor);
     }
